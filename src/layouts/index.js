239da8b9ecc4a2a6704f15aa4b8a929566903600
Makoto <inouemak@googlemail.com>
2018-06-27 12:48:03 +0100

    Fixed Jarrad

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,7 @@ import Helmet from 'react-helmet'
 import Header from '../components/header/Header.js'
 import '../globalStyles'
 import './index.css'
+import favicon from '../assets/favicon.png';
 
 const Layout = ({ children, data }) => (
   <Fragment>
@@ -13,6 +14,9 @@ const Layout = ({ children, data }) => (
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
+      ]}
+      link={[
+        { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
       ]}
     />
     <Header siteTitle={data.site.siteMetadata.title} />
