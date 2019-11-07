0b31588ecc4b278c0240c40f32cbc228bbe92da3
Makoto <inouemak@googlemail.com>
2019-11-07 21:13:18 +0000

    Added echo

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,8 @@ DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
 echo $DEPLOY_DOMAIN
 echo `pwd`
 echo $DEPLOY_PATH
+echo `ls`
+echo `ls public`
 cp ${DEPLOY_PATH}/index.html ${DEPLOY_PATH}/200.html
 surge -p ${DEPLOY_PATH} -d $DEPLOY_DOMAIN
 GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
