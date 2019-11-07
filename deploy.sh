245233030e4d403d3376a3157a292a1537dbc3c3
Makoto <inouemak@googlemail.com>
2019-11-07 21:32:42 +0000

    Echo GITHUB_PR_COMMENTS

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,8 @@ REPO_NAME=${REPO_SLUG_ARRAY[1]}
 DEPLOY_PATH=public
 DEPLOY_SUBDOMAIN=pr${TRAVIS_PULL_REQUEST}
 DEPLOY_DOMAIN=https://${DEPLOY_SUBDOMAIN}-${REPO_NAME}-${REPO_OWNER}.surge.sh
-echo $DEPLOY_DOMAIN
-echo `pwd`
-echo $DEPLOY_PATH
-echo `ls`
-echo `ls public`
 cp ${DEPLOY_PATH}/index.html ${DEPLOY_PATH}/200.html
 surge -p ${DEPLOY_PATH} -d $DEPLOY_DOMAIN
 GITHUB_PR_COMMENTS=https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments
+echo $GITHUB_PR_COMMENTS
 curl -u "makoto:${GITHUB_API_TOKEN}" --request POST ${GITHUB_PR_COMMENTS} --data '{"body":"PR deployed at: '${DEPLOY_DOMAIN}'"}'
