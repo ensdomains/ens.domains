e31f78affe99ce2cdd63a59a3b38ef201d0011c5
Makoto <inouemak@googlemail.com>
2020-05-06 11:57:02 +0100

    Change from GITHUB_API_TOKEN to GITHUB_TOKEN

--- deploy-gh.sh
+++ deploy-gh.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # From https://medium.com/onfido-tech/travis-surge-github-auto-deploy-every-pr-branch-and-tag-a6c8c790831f
-gh-pages -d public -r "https://ensbot:${GITHUB_API_TOKEN}@github.com/ensdomains/ens.domains.git"
+gh-pages -d public -r "https://ensbot:${GITHUB_TOKEN}@github.com/ensdomains/ens.domains.git"
